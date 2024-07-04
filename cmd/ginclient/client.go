d4fbe0c01827003764896ecc7ef4f3df11b8c1a2
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-07-04 16:36:06 +0300

    feat: basic client implementation, adding repo initials, gitactions, gitignore, go.mod

---
+++ cmd/ginclient/client.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	c := http.Client{Timeout: time.Duration(1) * time.Second}
+	response, err := c.Get("http://localhost:8089/datetime")
+	if err != nil {
+		fmt.Printf("Error %s", err)
+		return
+	}
+	defer response.Body.Close()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Printf("Body : %s", body)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/codescalersinternships/DateTime-HTTP-Client-Rowan
+
+go 1.22.4
